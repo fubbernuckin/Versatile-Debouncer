62eeba9e3eb5427a9304b590abb52f311bb81204
Cameron Ramirez <ramirez.cameron@gmail.com>
2025-12-29 20:50:04 -0600

    rising and falling edge detection is way more efficient, readme still needs updating.

--- Src/debounce.c
+++ Src/debounce.c
@@ -2,15 +2,14 @@
 #include <stdbool.h>
 #include <debounce.h>
 
-enum _state_bit_mask {
+const enum _state_bit_mask {
 	curr_state = 0x01,
-	prev_state = 0x02,
-	falling_edge = 0x04,
-	rising_edge = 0x08
+	falling_edge = 0x02,
+	rising_edge = 0x04
 };
 
 void DB_Init(DB_Handle *db, DB_Button *buttons, uint8_t count, DB_GPIO_Read rd) {
-	for(int i = 0; i < count; i++) {
+	for (int i = 0; i < count; i++) {
 		bool in = rd(buttons[i].pin);
 		buttons[i]._state = in;
 		buttons[i]._counter = in * buttons[i].threshold;
@@ -23,47 +22,36 @@ void DB_Init(DB_Handle *db, DB_Button *buttons, uint8_t count, DB_GPIO_Read rd) 
 void DB_Update(DB_Handle *db) {
 	/*
 	 * _state stores different flags in its bits
-	 * 0b0000abcd
+	 * 0b00000abc
 	 * a: rising edge latch
 	 * b: falling edge latch
-	 * c: previous state
-	 * d: current state
+	 * c: current state
 	 * 0: undefined
 	 */
-	for(int i = 0; i < db->count; i++) {
+	for (int i = 0; i < db->count; i++) {
 		DB_Button *btn = &(db->btns[i]);
 
-		btn->_state = (((btn->_state & curr_state) << 1) | (btn->_state & (~prev_state))); // set history bit while maintaining other bits
-
 		// perform debounce update
-		if(db->rd(btn->pin)) {
-			if(btn->_counter < btn->threshold) {
+		if (db->rd(btn->pin)) {
+			if (btn->_counter < btn->threshold) {
 				btn->_counter++;
 			}
 			else {
+				if ((btn->_state & curr_state) == 0) {
+					btn->_state = btn->_state | rising_edge; // set rising edge bit
+				}
 				btn->_state = btn->_state | curr_state; // set state bit
 			}
 		}
 		else {
-			if(btn->_counter != 0) {
+			if (btn->_counter != 0) {
 				btn->_counter--;
 			}
 			else {
+				if ((btn->_state & curr_state) == 1) {
+					btn->_state = btn->_state | falling_edge; // set falling edge bit
+				}
 				btn->_state = btn->_state & ~curr_state; // clear state bit
-			}
-		}
-
-		//set rising and falling edge flags
-		if((btn->_state & curr_state) !=0) {
-			if((btn->_state & prev_state) == 0) {
-				// rising edge
-				btn->_state = btn->_state | rising_edge;
-			}
-		}
-		else {
-			if((btn->_state & prev_state) !=0) {
-				// falling edge
-				btn->_state = btn->_state | falling_edge;
 			}
 		}
 	}
@@ -74,7 +62,7 @@ bool DB_Rd(const DB_Button *btn) {
 }
 
 bool DB_Rising(DB_Button *btn) {
-	if((btn->_state & rising_edge) != 0) {
+	if ((btn->_state & rising_edge) != 0) {
 		btn->_state = btn->_state & ~rising_edge; // clear rising edge bit
 		return true;
 	}
@@ -82,7 +70,7 @@ bool DB_Rising(DB_Button *btn) {
 }
 
 bool DB_Falling(DB_Button *btn) {
-	if((btn->_state & falling_edge) != 0) {
+	if ((btn->_state & falling_edge) != 0) {
 		btn->_state = btn->_state & ~falling_edge; // clear falling edge bit
 		return true;
 	}
